c03c06b93c7d999574d87983bb6c9c8a747352f9
seipan1213 <seiju1213@gmail.com>
2022-06-08 22:45:59 +0900

    ABC253 C

--- ABC253/c.cpp
+++ ABC253/c.cpp
@@ -9,4 +9,27 @@ const int INF = 1e9;
 
 int main()
 {
+	int q, cmd, x, c;
+	cin >> q;
+	map<ll, int> mp;
+	rep(i, q)
+	{
+		cin >> cmd;
+		if (cmd == 1)
+		{
+			cin >> x;
+			mp[x]++;
+		}
+		else if (cmd == 2)
+		{
+			cin >> x >> c;
+			mp[x] -= min(mp[x], c);
+			if (mp[x] == 0)
+				mp.erase(x);
+		}
+		else if (cmd == 3)
+		{
+			cout << mp.rbegin()->first - mp.begin()->first << endl;
+		}
+	}
 }
